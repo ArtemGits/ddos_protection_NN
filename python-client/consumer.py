18af7dd3ee28d8eeb3f885a2ba2e3630b120b3d1
agits <gitsartem@gmail.com>
2019-03-31 23:32:40 +0300

    implement black_list

--- python-client/consumer.py
+++ python-client/consumer.py
@@ -41,39 +41,6 @@ def model_load():
 
 
 def make_prediction(model, dataset):
-    labels = ['Flow ID',	'Src IP',	'Src Port',	'Dst IP',	'Dst Port',
-              'Protocol', 'Timestamp',	'Flow Duration',	'Tot Fwd Pkts',
-              'Tot Bwd Pkts',	'TotLen Fwd Pkts',	'TotLen Bwd Pkts',
-              'Fwd Pkt Len Max',	'Fwd Pkt Len Min',	'Fwd Pkt Len Mean',
-              'Fwd Pkt Len Std',	'Bwd Pkt Len Max',	'Bwd Pkt Len Min',
-              'Bwd Pkt Len Mean',	'Bwd Pkt Len Std',	'Flow Byts/s',
-              'Flow Pkts/s',	'Flow IAT Mean',	'Flow IAT Std',
-              'Flow IAT Max',	'Flow IAT Min',	'Fwd IAT Tot',	'Fwd IAT Mean',
-              'Fwd IAT Std',	'Fwd IAT Max',	'Fwd IAT Min',	'Bwd IAT Tot',
-              'Bwd IAT Mean',	'Bwd IAT Std',	'Bwd IAT Max',	'Bwd IAT Min',
-              'Fwd PSH Flags',	'Bwd PSH Flags',	'Fwd URG Flags',
-              'Bwd URG Flags',	'Fwd Header Len',	'Bwd Header Len',
-              'Fwd Pkts/s',
-              'Bwd Pkts/s',	'Pkt Len Min',	'Pkt Len Max',	'Pkt Len Mean',
-              'Pkt Len Std',
-              'Pkt Len Var',	'FIN Flag Cnt',	'SYN Flag Cnt',	'RST Flag Cnt',
-              'PSH Flag Cnt',	'ACK Flag Cnt', 'URG Flag Cnt',	'CWE Flag Count',
-              'ECE Flag Cnt',	'Down/Up Ratio',	'Pkt Size Avg',
-              'Fwd Seg Size Avg', 'Bwd Seg Size Avg',	'Fwd Byts/b Avg',
-              'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg',	'Bwd Byts/b Avg',
-              'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg',	'Subflow Fwd Pkts',
-              'Subflow Fwd Byts', 'Subflow Bwd Pkts',	'Subflow Bwd Byts',
-              'Init Fwd Win Byts', 'Init Bwd Win Byts'	'Fwd Act Data Pkts',
-              'Fwd Seg Size Min', 'Active Mean',	'Active Std',
-              'Active Max',	'Active Min', 'Idle Mean',	'Idle Std',
-              'Idle Max',	'Idle Min']
-
-    # print(len(labels))
-
-    # df = pd.DataFrame.from_records(data, columns=labels)
-
-    # dataset = df.values
-    # print(dataset)
     print("---------------")
 
     flow_id = np.array(dataset[:, 0]).reshape(-1, 1)
@@ -99,8 +66,22 @@ def make_prediction(model, dataset):
     # print(len(X_data[0]))
     # print(X_data.shape)
     classes = model.predict(X_data, batch_size=1)
+    # print(dataset)
+    # print(classes)
+    # print(classes.shape)
+    #print(dataset[..., 83].shape)
+    classes = classes.reshape(-1)
+    # print(classes)
+    # print(classes.shape)
+    dataset[..., 83] = classes
 
-    print(classes)
+    # print(dataset)
+    black_list = list(set([x[0] for x in dataset[:, [1, 83]] if x[1] >= .5]))
+    print(black_list)
+    with open('black_list/black_list.txt', 'w') as f:
+        for ip in black_list:
+            f.write("%s\n" % ip)
+
 
 
 def kafka_setup():
